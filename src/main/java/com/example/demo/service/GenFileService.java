ea1ca2570b94b4f65db6346300d672e4aec808d4
nwh1124 <nwh0716@gmail.com>
2021-03-03 12:14:12 +0900

    지정된 디렉토리에 파일을 업로드하는 기능

--- src/main/java/com/example/demo/service/GenFileService.java
+++ src/main/java/com/example/demo/service/GenFileService.java
@@ -1,9 +1,13 @@
 package com.example.demo.service;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.example.demo.dao.GenFileDao;
 import com.example.demo.dto.ResultData;
@@ -11,6 +15,9 @@ import com.example.demo.util.Util;
 
 @Service
 public class GenFileService {
+	@Value("${custom.genFileDirPath}")
+	private String genFileDirPath;
+	
 	@Autowired
 	private GenFileDao genFileDao;
 
@@ -27,5 +34,62 @@ public class GenFileService {
 		int id = Util.getAsInt(param.get("id"), 0);
 		return new ResultData("S-1", "성공하였습니다.", "id", id);
 		
+	}
+
+	public ResultData save(MultipartFile multipartFile, int relId) {
+		String fileInputName = multipartFile.getName();
+		String[] fileInputNameBits = fileInputName.split("__");
+		
+		if(fileInputNameBits[0].equals("file") == false) {
+			return new ResultData("F-1", "파라미터 명이 올바르지 않습니다.");
+		}
+		
+		int fileSize = (int) multipartFile.getSize();
+		
+		if(fileSize <= 0) {
+			return new ResultData("F-2", "파일이 업로드되지 않았습니다."); 
+		}
+		
+		String relTypeCode = fileInputNameBits[1];
+		String typeCode = fileInputNameBits[3];
+		String type2Code = fileInputNameBits[4];
+		int fileNo = Integer.parseInt(fileInputNameBits[5]);
+		String originFileName = multipartFile.getOriginalFilename();
+		String fileExtTypeCode = Util.getFileExtTypeCodeFromFileName(multipartFile.getOriginalFilename());
+		String fileExtType2Code = Util.getFileExtType2CodeFromFileName(multipartFile.getOriginalFilename());
+		String fileExt = Util.getFileExtFromFileName(multipartFile.getOriginalFilename()).toLowerCase();
+		
+		if(fileExt.equals("jpeg")) {
+			fileExt = "jpg";
+		}else if(fileExt.equals("htm")) {
+			fileExt = "html";
+		}
+		
+		String fileDir = Util.getNowYearMonthDateStr();
+		
+		ResultData saveMetaRd = saveMeta(relTypeCode, relId, typeCode, type2Code, fileNo, originFileName,
+				fileExtTypeCode, fileExtType2Code, fileExt, fileSize, fileDir);
+		
+		int newGenFileId = (int)saveMetaRd.getBody().get("id");
+		
+		// 새 파일이 저장될 폴더 객체 생성
+		String targetDirPath = genFileDirPath + "/" + relTypeCode + "/" + fileDir;
+		java.io.File targetDir = new java.io.File(targetDirPath);
+		
+		if(targetDir.exists() == false) {
+			targetDir.mkdirs();
+		}
+		
+		String targetFileName = newGenFileId + "." + fileExt;
+		String targetFilePath = targetDirPath + "/" + targetFileName;
+		
+		try {
+			multipartFile.transferTo(new File(targetFilePath));
+		} catch (IllegalStateException | IOException e) {
+			e.printStackTrace();
+			return new ResultData("F-3", "파일 저장에 실패했습니다.");
+		} 
+		
+		return new ResultData("S-1", "파일이 생성되었습니다.", "id", newGenFileId, "fileRealPath", targetFilePath, "fileName", targetFileName);
 	}
 }
